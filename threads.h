9c847ed72588d22452fbe1fc0c10cab90f11e86a
rdougla3 <dougl@DESKTOP-VQ2JVTO.localdomain>
2020-03-08 13:04:35 -0700

    step 3

--- threads.h
+++ threads.h
@@ -1,6 +1,8 @@
 #include "q.h"
 
 struct Q* RunQ;
+struct TCB_t* Curr_Thread;
+struct TCB_t* Prev_Thread;
 
 void start_thread(void (*function) (void)){
     int* stack = malloc(8192); //allocate a stack of 8192 bytes
@@ -8,4 +10,18 @@ void start_thread(void (*function) (void)){
     init_TCB(TCB, function, stack, sizeof(stack)); //call init_TCB 
     TCB->thread_id = ++global_thread_id; //initialize thread_id
     AddQueue(RunQ, TCB); //call addQ to add this TCB into the "RunQ"
+}
+
+void run(){   // real code
+    Curr_Thread = RunQ->head;
+    ucontext_t parent; // get a place to store the main context, for faking
+    getcontext(&parent); // magic sauce
+    swapcontext(&parent, &(Curr_Thread->context)); // start the first thread
+}
+
+void yield(){
+    Prev_Thread = RunQ->head;
+    RotateQ(&RunQ);
+    Curr_Thread = RunQ->head;
+    swapcontext(&(Prev_Thread->context), &(Curr_Thread->context)); //swap the context, from Prev_Thread to the thread pointed to by Curr_Thread
 }
